d38eb2e7c299c907139e0257b0d406fb8063fd97
Morris Chao <dreamaker0121@gmai.com>
2025-06-17 20:40:34 +0800

    250617_revised

--- code/2025_new/control_encode.vh
+++ code/2025_new/control_encode.vh
@@ -1,9 +1,11 @@
 `ifndef CONTROL_ENCODE_VH
 `define CONTROL_ENCODE_VH
 
+// -----------------------------------------------------------------------------
 // Bit positions for control word [21:0]
+// -----------------------------------------------------------------------------
 `define CTRL_HI_WRITE    21
-`define CTRL_HI_READ     10
+`define CTRL_HI_READ     20
 `define CTRL_LO_WRITE    19
 `define CTRL_LO_READ     18
 `define CTRL_IFUNSIGNED  17
@@ -17,11 +19,13 @@
 `define CTRL_JUMP         9
 `define CTRL_LINKED       8
 `define CTRL_RETURN       7
-`define CTRL_ALUOP      6:3
+`define CTRL_ALUOP       6:3
 `define CTRL_SHIFTV       2
-`define CTRL_DATA_TYPE  1:0
+`define CTRL_DATA_TYPE    1:0
 
+// -----------------------------------------------------------------------------
 // ALU operation codes
+// -----------------------------------------------------------------------------
 `define ALU_AND    4'd0
 `define ALU_OR     4'd1
 `define ALU_XOR    4'd2
@@ -39,9 +43,74 @@
 `define ALU_MUL    4'd14
 `define ALU_DIV    4'd15
 
-// data to data_mem type
+// -----------------------------------------------------------------------------
+// Data type constants
+// -----------------------------------------------------------------------------
 `define DATA_TYPE_WORD  2'b00
 `define DATA_TYPE_HALF  2'b01
 `define DATA_TYPE_BYTE  2'b10
+
+// -----------------------------------------------------------------------------
+// Control word constants for each instruction
+// (These were generated from previous mappings)
+// -----------------------------------------------------------------------------
+
+// Control signals for instruction
+`define CTRL_J     22'b0000000000001000000000
+`define CTRL_JAL   22'b0000000010001100000000
+`define CTRL_BEQ   22'b0000000000010000110000
+`define CTRL_BNE   22'b0000000000010000110000
+`define CTRL_BLEZ  22'b0000000000010001010000
+`define CTRL_BGTZ  22'b0000000000010000111000
+`define CTRL_ADDI  22'b0000001010000000100000
+`define CTRL_ADDIU 22'b0000101010000000100000
+`define CTRL_SLTI  22'b0000001010000001000000
+`define CTRL_SLTIU 22'b0000101010000001000000
+`define CTRL_ANDI  22'b0000001010000000000000
+`define CTRL_ORI   22'b0000001010000000001000
+`define CTRL_XORI  22'b0000001010000000010000
+`define CTRL_LUI   22'b0000001010000000000000
+`define CTRL_LB    22'b0000001111000000100010
+`define CTRL_LH    22'b0000001111000000100001
+`define CTRL_LWL   22'b0000001111000000100000
+`define CTRL_LW    22'b0000001111000000100000
+`define CTRL_LBU   22'b0000101111000000100010
+`define CTRL_LHU   22'b0000101111000000100001
+`define CTRL_LWR   22'b0000001111000000100000
+`define CTRL_SB    22'b0000001000100000100010
+`define CTRL_SH    22'b0000001000100000100001
+`define CTRL_SWL   22'b0000001000100000100000
+`define CTRL_SW    22'b0000001000100000100000
+`define CTRL_SWR   22'b0000001000100000100000
+`define CTRL_BLTZ  22'b0000000000010000101000
+`define CTRL_SLL   22'b0000010010000001101000
+`define CTRL_SRL   22'b0000010010000001011000
+`define CTRL_SRA   22'b0000010010000001100000
+`define CTRL_SLLV  22'b0000010010000001101100
+`define CTRL_SRLV  22'b0000010010000001011100
+`define CTRL_SRAV  22'b0000010010000001100100
+`define CTRL_JR    22'b0000000000001010000000
+`define CTRL_JALR  22'b0000010010001110000000
+`define CTRL_MFHI  22'b0100010010000000000000
+`define CTRL_MTHI  22'b1000000000000000000000
+`define CTRL_MFLO  22'b0001010010000000000000
+`define CTRL_MTLO  22'b0010000000000000000000
+`define CTRL_MULT  22'b1010010000000001110000
+`define CTRL_MULTU 22'b1010110000000001110000
+`define CTRL_DIV   22'b1010010000000001111000
+`define CTRL_DIVU  22'b1010110000000001111000
+`define CTRL_ADD   22'b0000010010000000100000
+`define CTRL_ADDU  22'b0000110010000000100000
+`define CTRL_SUB   22'b0000010010000000101000
+`define CTRL_SUBU  22'b0000110010000000101000
+`define CTRL_AND   22'b0000010010000000000000
+`define CTRL_OR    22'b0000010010000000001000
+`define CTRL_XOR   22'b0000010010000000010000
+`define CTRL_NOR   22'b0000010010000000011000
+`define CTRL_SLT   22'b0000010010000001000000
+`define CTRL_SLTU  22'b0000110010000001000000
+
+
+
 
 `endif
--- code/2025_new/control_unit.v
+++ code/2025_new/control_unit.v
@@ -1,370 +1,79 @@
-// -----------------------------------------------------------------------------
-// Control Unit Template for MIPS32
-// -----------------------------------------------------------------------------
-`include "Control_encode.vh"  // Contains CTRL_* and ALU_* defines
-`include "Mips32_ISATbl.vh"  // Contains OP_* and FN_* defines
+`include "Control_encode.vh"
+`include "Mips32_ISATbl.vh"
 
 module Control_unit (
     input  wire [5:0] opcode,
     input  wire [5:0] funct,
     input  wire [4:0] rt,
-    output wire  [21:0] control_word
+    output reg  [21:0] control_word
 );
 
-    reg HI_WRITE;
-    reg HI_READ;
-    reg LO_WRITE;
-    reg LO_READ;
-    reg IFUNSIGNED;
-    reg REGDST;
-    reg ALUSRC;
-    reg MEMTOREG;
-    reg REGWRITE;
-    reg MEMREAD;
-    reg MEMWRITE;
-    reg BRANCH;
-    reg JUMP;
-    reg LINKED;
-    reg RETURN;
-    reg [3:0]ALUOP;
-    reg SHIFT_V;
-    reg [1:0]DATA_TYPE;
-
-
-    // HI_WRITE
-    always @(*) begin
-        if (opcode == `OP_SPECIAL &&
-            (funct == `FN_MTHI || funct == `FN_MULT ||
-            funct == `FN_MULTU || funct == `FN_DIV ||
-            funct == `FN_DIVU )) begin
-            HI_WRITE = 1'b1; // HI_WRITE
-        end else begin
-            HI_WRITE = 1'b0; // HI_WRITE
-        end
-    end
-
-    // HI_READ
-    always @(*) begin
-        if(opcode == `OP_SPECIAL &&
-            (funct == `FN_MFHI)) begin
-            HI_READ = 1'b1; // HI_READ
-        end else begin
-            HI_READ = 1'b0; // HI_READ
-        end
-    end
-
-    // LO_WRITE
-    always @(*) begin
-        if (opcode == `OP_SPECIAL &&
-            (funct == `FN_MTLO || funct == `FN_MULT ||
-            funct == `FN_MULTU || funct == `FN_DIV ||
-            funct == `FN_DIVU)) begin
-            LO_WRITE = 1'b1; // LO_WRITE
-        end else begin
-            LO_WRITE = 1'b0; // LO_WRITE
-        end
-    end
-
-    // LO_READ
-    always @(*) begin
-        if(opcode == `OP_SPECIAL &&
-            (funct == `FN_MFLO)) begin
-            LO_READ = 1'b1; // LO_READ
-        end else begin
-            LO_READ = 1'b0; // LO_READ
-        end
-    end
-
-    // IFUNSIGNED
-    // 0: signed, 1: unsigned
     always @(*) begin
         case (opcode)
-            //i type
-            `OP_ADDIU,
-            `OP_SLTIU,
-            `OP_LBU,
-            `OP_LHU:     IFUNSIGNED = 1'b1;
-
-            //r type
-            `OP_SPECIAL: begin
-                case (funct)
-                    `FN_SLTU,
-                    `FN_ADDU, `FN_SUBU,
-                    `FN_MULTU,
-                    `FN_DIVU: IFUNSIGNED = 1'b1;
-                    default:  IFUNSIGNED = 1'b0;
+            `OP_J:       control_word = `CTRL_J;
+            `OP_JAL:     control_word = `CTRL_JAL;
+            `OP_BEQ:     control_word = `CTRL_BEQ;
+            `OP_BNE:     control_word = `CTRL_BNE;
+            `OP_BLEZ:    control_word = `CTRL_BLEZ;
+            `OP_BGTZ:    control_word = `CTRL_BGTZ;
+            `OP_ADDI:    control_word = `CTRL_ADDI;
+            `OP_ADDIU:   control_word = `CTRL_ADDIU;
+            `OP_SLTI:    control_word = `CTRL_SLTI;
+            `OP_SLTIU:   control_word = `CTRL_SLTIU;
+            `OP_ANDI:    control_word = `CTRL_ANDI;
+            `OP_ORI:     control_word = `CTRL_ORI;
+            `OP_XORI:    control_word = `CTRL_XORI;
+            `OP_LUI:     control_word = `CTRL_LUI;
+            `OP_LB:      control_word = `CTRL_LB;
+            `OP_LH:      control_word = `CTRL_LH;
+            `OP_LWL:     control_word = `CTRL_LWL;
+            `OP_LW:      control_word = `CTRL_LW;
+            `OP_LBU:     control_word = `CTRL_LBU;
+            `OP_LHU:     control_word = `CTRL_LHU;
+            `OP_LWR:     control_word = `CTRL_LWR;
+            `OP_SB:      control_word = `CTRL_SB;
+            `OP_SH:      control_word = `CTRL_SH;
+            `OP_SWL:     control_word = `CTRL_SWL;
+            `OP_SW:      control_word = `CTRL_SW;
+            `OP_SWR:     control_word = `CTRL_SWR;
+            `OP_REGIMM: begin
+                case (rt)
+                    `RT_BLTZ: control_word = `CTRL_BLTZ;
+                    default:  control_word = 22'b0;
                 endcase
             end
-
-            default: IFUNSIGNED = 1'b0;
-        endcase
-    end
-
-
-
-    // REGDST
-    // 0: rt, 1: rd
-    always @(*) begin
-        case (opcode)
-            // R-type instructions
             `OP_SPECIAL: begin
                 case (funct)
-                    `FN_JR:
-                        REGDST = 1'b0;
-                    `FN_MTHI, `FN_MTLO:
-                        REGDST = 1'b0; 
-                    default:
-                        REGDST = 1'b1; // R-type uses rd
+                    `FN_SLL:   control_word = `CTRL_SLL;
+                    `FN_SRL:   control_word = `CTRL_SRL;
+                    `FN_SRA:   control_word = `CTRL_SRA;
+                    `FN_SLLV:  control_word = `CTRL_SLLV;
+                    `FN_SRLV:  control_word = `CTRL_SRLV;
+                    `FN_SRAV:  control_word = `CTRL_SRAV;
+                    `FN_JR:    control_word = `CTRL_JR;
+                    `FN_JALR:  control_word = `CTRL_JALR;
+                    `FN_MFHI:  control_word = `CTRL_MFHI;
+                    `FN_MTHI:  control_word = `CTRL_MTHI;
+                    `FN_MFLO:  control_word = `CTRL_MFLO;
+                    `FN_MTLO:  control_word = `CTRL_MTLO;
+                    `FN_MULT:  control_word = `CTRL_MULT;
+                    `FN_MULTU: control_word = `CTRL_MULTU;
+                    `FN_DIV:   control_word = `CTRL_DIV;
+                    `FN_DIVU:  control_word = `CTRL_DIVU;
+                    `FN_ADD:   control_word = `CTRL_ADD;
+                    `FN_ADDU:  control_word = `CTRL_ADDU;
+                    `FN_SUB:   control_word = `CTRL_SUB;
+                    `FN_SUBU:  control_word = `CTRL_SUBU;
+                    `FN_AND:   control_word = `CTRL_AND;
+                    `FN_OR:    control_word = `CTRL_OR;
+                    `FN_XOR:   control_word = `CTRL_XOR;
+                    `FN_NOR:   control_word = `CTRL_NOR;
+                    `FN_SLT:   control_word = `CTRL_SLT;
+                    `FN_SLTU:  control_word = `CTRL_SLTU;
+                    default:   control_word = 22'b0;
                 endcase
             end
-
-            default:
-                REGDST = 1'b0; // I-type uses rt
+            default: control_word = 22'b0;
         endcase
     end
-
-
-    // ALUSRC
-    // 0: reg_out
-    // 1: immediate
-    always @(*) begin
-        case (opcode)
-            `OP_ADDI, `OP_ADDIU,
-            `OP_ANDI, `OP_ORI, `OP_XORI,
-            `OP_SLTI, `OP_SLTIU,
-            `OP_LUI,
-            `OP_LB, `OP_LH, `OP_LW, `OP_LBU, `OP_LHU, `OP_LWL, `OP_LWR,
-            `OP_SB, `OP_SH, `OP_SW, `OP_SWL, `OP_SWR:
-                ALUSRC = 1'b1; // Use immediate for I-type and load/store
-            default:
-                ALUSRC = 1'b0; // Use register for R-type
-        endcase
-    end
-
-    // MEMTOREG
-    // 0: ALU_out
-    // 1: data (memory read)
-    always @(*) begin
-        if (opcode == `OP_LB  || opcode == `OP_LH  || opcode == `OP_LW  ||
-            opcode == `OP_LBU || opcode == `OP_LHU || opcode == `OP_LWL ||
-            opcode == `OP_LWR) begin
-            MEMTOREG = 1'b1; // Load instructions write memory data to register
-        end else begin
-            MEMTOREG = 1'b0; // Otherwise, write ALU result to register
-        end
-    end
-
-    // REGWRITE
-    // 0: false (do not write to register file)
-    // 1: true  (write to register file)
-    always @(*) begin
-        case (opcode)
-            // R-type: only MFHI, MFLO, and others like ADD/SUB write to register
-            `OP_SPECIAL: begin
-                case (funct)
-                    `FN_JR,
-                    `FN_MTHI, `FN_MTLO,
-                    `FN_MULT, `FN_MULTU,
-                    `FN_DIV,  `FN_DIVU:
-                        REGWRITE = 1'b0;
-
-                    default:
-                        REGWRITE = 1'b1; // other R-type
-                endcase
-            end
-
-            // I-type: write-back instructions
-            `OP_ADDI, `OP_ADDIU,
-            `OP_ANDI, `OP_ORI, `OP_XORI,
-            `OP_SLTI, `OP_SLTIU,
-            `OP_LUI,
-            `OP_LB, `OP_LH, `OP_LW, `OP_LBU, `OP_LHU, `OP_LWL, `OP_LWR:
-                REGWRITE = 1'b1;
-
-            // J-type with link (JAL writes to $ra)
-            `OP_JAL:
-                REGWRITE = 1'b1;
-
-            default:
-                REGWRITE = 1'b0;
-        endcase
-    end
-
-
-
-    // MEMREAD
-    // 0: false (do not read memory)
-    // 1: true  (read memory)
-    always @(*) begin
-        case (opcode)
-            `OP_LB, `OP_LH, `OP_LW,
-            `OP_LBU, `OP_LHU,
-            `OP_LWL, `OP_LWR:
-                MEMREAD = 1'b1; // Load instructions require memory read
-
-            default:
-                MEMREAD = 1'b0;
-        endcase
-    end
-
-    // MEMWRITE
-    // 0: false (do not write memory)
-    // 1: true  (write memory)
-    always @(*) begin
-        case (opcode)
-            // Store instructions
-            `OP_SB, `OP_SH, `OP_SW, `OP_SWL, `OP_SWR:
-                MEMWRITE = 1'b1;
-            default:
-                MEMWRITE = 1'b0;
-        endcase
-    end
-
-    // BRANCH
-    // 0: false (do not branch)
-    // 1: true  (branch)
-    always @(*) begin
-        case (opcode)
-            //branch instructions
-            `OP_BEQ, `OP_BNE, `OP_BGTZ, `OP_BLEZ:
-                BRANCH = 1'b1;
-            `OP_REGIMM:begin
-                if(rt==`RT_BLTZ)
-                    BRANCH = 1'b1; // BLTZ  
-                else
-                    BRANCH = 1'b0; // other regimm instructions do not branch 
-            end
-            default:
-                BRANCH = 1'b0;
-        endcase
-    end
-
-    // JUMP
-    // 0: false (do not jump)
-    // 1: true  (jump)
-    always @(*) begin
-        case (opcode)
-            `OP_J, `OP_JAL: JUMP = 1'b1;
-            `OP_SPECIAL: begin
-                case (funct)
-                    `FN_JR, `FN_JALR: JUMP = 1'b1;
-                    default: JUMP = 1'b0;
-                endcase
-            end
-            default: JUMP = 1'b0;
-        endcase
-    end
-
-    // LINKED
-    // 0: false (not linked)
-    // 1: true  (linked, e.g., JAL)
-    always @(*) begin
-        if (opcode == `OP_JAL || (opcode == `OP_SPECIAL && funct == `FN_JALR))
-            LINKED = 1'b1;
-        else
-            LINKED = 1'b0;
-    end
-
-    // RETURN
-    // 0: false (not returning)
-    // 1: true  (returning, e.g., JR)
-    always @(*) begin
-        if (opcode == `OP_SPECIAL && (funct == `FN_JR || funct == `FN_JALR))
-            RETURN = 1'b1;
-        else
-            RETURN = 1'b0;
-    end
-
-    // ALUOP
-    always @(*) begin
-        case (opcode)
-            // R-type instructions
-            `OP_SPECIAL: begin
-                case (funct)
-                    `FN_ADD, `FN_ADDU:       ALUOP = `ALU_ADD;
-                    `FN_SUB, `FN_SUBU:       ALUOP = `ALU_SUB;
-                    `FN_AND:                 ALUOP = `ALU_AND;
-                    `FN_OR:                  ALUOP = `ALU_OR;
-                    `FN_XOR:                 ALUOP = `ALU_XOR;
-                    `FN_NOR:                 ALUOP = `ALU_NOR;
-                    `FN_SLT:                 ALUOP = `ALU_LT;
-                    `FN_SLTU:                ALUOP = `ALU_LT;
-                    `FN_SLL, `FN_SLLV:       ALUOP = `ALU_SLL;
-                    `FN_SRL, `FN_SRLV:       ALUOP = `ALU_SRL;
-                    `FN_SRA, `FN_SRAV:       ALUOP = `ALU_SRA;
-                    `FN_MULT, `FN_MULTU:     ALUOP = `ALU_MUL;
-                    `FN_DIV, `FN_DIVU:       ALUOP = `ALU_DIV;
-                    default:                 ALUOP = 4'd0;
-                endcase
-            end
-
-            // I-type arithmetic instructions
-            `OP_ADDI, `OP_ADDIU,
-            `OP_LW, `OP_LB, `OP_LH, `OP_LBU, `OP_LHU, `OP_LWL, `OP_LWR,
-            `OP_SW, `OP_SB, `OP_SH, `OP_SWL, `OP_SWR:
-                ALUOP = `ALU_ADD;
-            `OP_LUI:      ALUOP = `ALU_AND; // Load Upper Immediate
-            `OP_ANDI:     ALUOP = `ALU_AND;
-            `OP_ORI:      ALUOP = `ALU_OR;
-            `OP_XORI:     ALUOP = `ALU_XOR;
-            `OP_SLTI:     ALUOP = `ALU_LT;
-            `OP_SLTIU:    ALUOP = `ALU_LT;
-
-            // Branch instructions
-            `OP_BEQ, `OP_BNE:
-                ALUOP = `ALU_EQ;
-
-            `OP_BLEZ:
-                ALUOP = `ALU_LE;
-
-            `OP_BGTZ:
-                ALUOP = `ALU_GT;
-
-            `OP_REGIMM: // Branch on register
-            begin
-                if(rt==`RT_BLTZ)
-                    ALUOP = `ALU_SUB; // BLTZ
-            end
-
-            default:
-                ALUOP = 4'd0;
-        endcase
-    end
-
-    // SHIFT_V
-    // 0: shamt, 1: rs
-    always @(*) begin
-    //SLLV SRLV SRAV
-        if(opcode==`OP_SPECIAL &&(funct==`FN_SLLV || funct==`FN_SRLV || funct==`FN_SRAV)) begin
-            SHIFT_V = 1'b1; // rs
-        end else begin
-            SHIFT_V = 1'b0; // shamt
-        end
-    end
-
-    // DATA_TYPE
-    // 00: word, 01: half, 10: byte
-    always @(*) begin
-        case (opcode)
-            // Byte
-            `OP_LB, `OP_LBU, `OP_SB:
-                DATA_TYPE = 2'b10;
-
-            // Halfword
-            `OP_LH, `OP_LHU, `OP_SH:
-                DATA_TYPE = 2'b01;
-
-            default:
-                DATA_TYPE = 2'b00; //defalut word
-        endcase
-    end
-
-
-    // output control word
-    assign control_word = {
-        HI_WRITE, HI_READ, LO_WRITE, LO_READ,
-        IFUNSIGNED, REGDST, ALUSRC, MEMTOREG,
-        REGWRITE, MEMREAD, MEMWRITE, BRANCH,
-        JUMP, LINKED, RETURN, ALUOP, SHIFT_V, DATA_TYPE
-    };
 endmodule
